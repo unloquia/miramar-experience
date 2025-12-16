0cf0d348345d2a38828e38f5f63af1925af65164
unloquia <unloquia@gmail.com>
2025-12-16 18:07:43 -0300

    feat: Mapas Limpios (CartoDB Positron)
    - Reemplazado OpenStreetMap estandar por CartoDB Positron en Landing y Admin.
    - Elimina ruido visual (comercios y POIs ajenos) para resaltar los marcadores de la app.
    - Estilo muchisimo mas profesional y limpio.

--- src/components/admin/LocationPicker.tsx
+++ src/components/admin/LocationPicker.tsx
@@ -219,8 +219,8 @@ export function LocationPicker({
                             className="z-0"
                         >
                             <TileLayer
-                                attribution='&copy; OpenStreetMap'
-                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
                             />
 
                             {/* Click handler */}
--- src/components/landing/MapView.tsx
+++ src/components/landing/MapView.tsx
@@ -158,8 +158,8 @@ export function MapView({
                 style={{ height, width: '100%' }}
             >
                 <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                    url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
                 />
                 {L && filteredPlaces.map((place) => (
                     <Marker
